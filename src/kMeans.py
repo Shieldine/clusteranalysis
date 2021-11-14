fb20e641d3e817d8ab969adca2d8abed85c38fd0
Agnes Widera <agnes.widera@studmail.w-hs.de>
2021-11-14 16:47:01 +0100

    Add comments, add x and y-labels in plots

--- src/kMeans.py
+++ src/kMeans.py
@@ -8,7 +8,7 @@ class KMeans:
         self.max_iterations = max_iterations
         self.verbose = verbose
         self.centroids = []
-        self.k = 2
+        self.k = None
         self.data = None
         self.clusters = None
 
@@ -17,14 +17,17 @@ class KMeans:
         if include_centroids:
             for point in self.centroids:
                 plt.scatter(*point, marker='x', color='black')
+        plt.xlabel("X")
+        plt.ylabel("Y")
         plt.show()
 
     def create_random_centroids(self):
-        centroid_min_coordinate = self.data.min().min()
-        centroid_max_coordinate = self.data.max().max()
-        for centroid in range(self.k):
+        centroid_min_coordinate = self.data.min().min()  # find point with smallest coordinates
+        centroid_max_coordinate = self.data.max().max()  # find point with biggest coordinates
+        for centroid in range(self.k):  # repeat k-times
+            # create random coordinates with previously found smallest and biggest as borders
             centroid = np.random.uniform(centroid_min_coordinate, centroid_max_coordinate, self.data.shape[1])
-            self.centroids.append(centroid)
+            self.centroids.append(centroid)  # add created centroid to the list
 
     def calculate_euclidean_distance(self):
         pass
@@ -36,4 +39,4 @@ class KMeans:
 
         # initialize random centroids
         self.create_random_centroids()
-        self.plot(include_centroids=True)
+        self.plot(include_centroids=False)
